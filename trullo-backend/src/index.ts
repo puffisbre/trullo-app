60a5b0f6949a636da772218ee10b2cc3de66b6ff
FelixOS <puffis.stenersjo@gmail.com>
2025-11-17 13:06:20 +0100

    added dockerfile

--- trullo-backend/src/index.ts
+++ trullo-backend/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
